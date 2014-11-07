93c18a5716d888bf84d147465b38dbfefb38ffe3
riccardogit <riccardogit@D1_046000035.gruppo.autostrade.it>
2014-11-07 11:57:11 +0100

    variati gli spazi

--- ProvaGit/src/it/prova/git/ProvaGit.java
+++ ProvaGit/src/it/prova/git/ProvaGit.java
@@ -5,6 +5,7 @@ public class ProvaGit
 	public static final void main(String[] args)
 	{
 		System.out.println("Ciao mondo");
+		
 		System.out.println("Ciao mondo 2");
 	}
 }
--- ProvaGit/src/it/prova/git/ProvaGit2.java
+++ ProvaGit/src/it/prova/git/ProvaGit2.java
@@ -2,7 +2,6 @@ package it.prova.git;
 
 public class ProvaGit2
 {
-	
 	public static void main(String[] args)
 	{
 		System.out.println("Ciao mondo");

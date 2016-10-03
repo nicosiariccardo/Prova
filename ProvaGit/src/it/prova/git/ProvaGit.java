a321ddabdda3cecba75785681103782854add568
rnicosia <rnicosia@firenze.eng.it>
2016-10-03 09:40:11 +0200

    Rinizio le prove

--- ProvaGit/src/it/prova/git/ProvaGit.java
+++ ProvaGit/src/it/prova/git/ProvaGit.java
@@ -4,9 +4,7 @@ public class ProvaGit
 {
 	public static final void main(String[] args)
 	{
+		/*Riparto*/
 		System.out.println("Ciao mondo");
-		System.out.println("Ciao mondo 2");
-		
-		System.out.println("branchprova master");
 	}
 }

ccee78b6390dc1724411f84def0305ae12a01eae
rnicosia <rnicosia@firenze.eng.it>
2016-10-03 11:58:08 +0200

    Modifica post terzo stash, risolto conflitto

--- ProvaGit/src/it/prova/git/ProvaGit2.java
+++ ProvaGit/src/it/prova/git/ProvaGit2.java
@@ -7,5 +7,7 @@ public class ProvaGit2
 		System.out.println("Ciao mondo");
 		
 		System.out.println("Ciao mondo commit post stash ramo!!!");
+		
+		System.out.println("Aggiungo stash senza conflitto...con conflitto");
 	}
 }

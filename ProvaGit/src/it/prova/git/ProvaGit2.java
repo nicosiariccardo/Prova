07668de72324d1c48b7af015d3d4d01347fcb7c6
rnicosia <rnicosia@firenze.eng.it>
2016-10-12 11:15:58 +0200

    Modifica da remoto 8

--- ProvaGit/src/it/prova/git/ProvaGit2.java
+++ ProvaGit/src/it/prova/git/ProvaGit2.java
@@ -39,5 +39,11 @@ public class ProvaGit2
 		System.out.println("Conflitto con remoto 5");
 		
 		System.out.println("Modifica da remoto 5");
+		
+		System.out.println("Conflitto con remoto 6");
+		
+		System.out.println("Modifica da remoto 7");
+		
+		System.out.println("Modifica da remoto 8");
 	}
 }
